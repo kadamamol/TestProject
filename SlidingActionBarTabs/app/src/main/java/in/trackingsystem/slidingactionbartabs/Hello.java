3ebead5fcaf2329ec005fe936ac01258dd799fb6
Amol Kadam <amolkadam1718@hotmail.com>
2016-05-06 12:08:16 +0530

    Added data in class

--- SlidingActionBarTabs/app/src/main/java/in/trackingsystem/slidingactionbartabs/Hello.java
+++ SlidingActionBarTabs/app/src/main/java/in/trackingsystem/slidingactionbartabs/Hello.java
@@ -4,4 +4,9 @@ package in.trackingsystem.slidingactionbartabs;
  * Created by inscripts49 on 5/6/16.
  */
 public class Hello {
+
+    public Hello() {
+
+        System.out.println("Hello World");
+    }
 }
